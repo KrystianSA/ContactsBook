98fce808616998ed5deb811ce94007cf08fbbb58
Krystian Sąsiadek <122701689+KrystianSA@users.noreply.github.com>
2023-08-25 13:00:42 +0200

    Added Moq and Auto Fixture to tests. First tested GetContacts with this nugets

--- ContactsBook/ContactBookTests/ContactsControllerTests.cs
+++ ContactsBook/ContactBookTests/ContactsControllerTests.cs
@@ -1,9 +1,15 @@
-﻿using FluentAssertions;
+﻿using AutoFixture;
+using ContactsBook.Models;
+using FluentAssertions;
 using Microsoft.AspNetCore.Authorization.Policy;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Moq;
+using RecruitmentTask.Controllers;
 using RecruitmentTask.Data;
 using RecruitmentTask.Entities;
+using RecruitmentTask.Services;
+using System.ComponentModel;
 using WebAppTests.Extensions;
 using Xunit;
 
@@ -13,29 +19,32 @@ namespace ContactsBook.IntegrationTests
     {
         private HttpClient _client;
         private WebApplicationFactory<Program> _factory;
+        private Mock<IContactService> _mockContactService;
+        private Fixture _fixture;
 
         public ContactsControllerTests(WebApplicationFactory<Program> factory)
         {
+            _fixture = new Fixture();
+            _mockContactService = new Mock<IContactService>();
             _factory = factory
                 .WithWebHostBuilder(builder =>
                 {
                     builder.ConfigureServices(services =>
                     {
-                        var dbContextOptions = services.SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<DataDbContext>));
-                        services.Remove(dbContextOptions);
-                        services.AddDbContext<DataDbContext>(options => options.UseInMemoryDatabase("testDb"));
-
                         services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                        services.AddScoped<IContactService>(serviceProvider => _mockContactService.Object);
                     });
                 });
-
-            _client=_factory.CreateClient();
-
+            _client = _factory.CreateClient();
         }
+
+        //Spróbuj użyć MOQ to testów, szczególnie dla wyszukiwania elementów których nie ma
 
         [Fact]
         public async Task GetContacts_WithCorrectQuery_ReturnStatusOk()
         {
+            var contacts = _fixture.CreateMany<GetContactsDto>(3);
+            _mockContactService.Setup(service => service.GetAll()).Returns(contacts);
             var result = await _client.GetAsync("/contacts");
             result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
         }
@@ -43,22 +52,21 @@ namespace ContactsBook.IntegrationTests
         [Fact]
         public async Task GetContactById_ForExistId_ReturnStatusOk()
         {
-            var contact = new Contact()
-            {
-                Id = 2
-            };
-            var result = await _client.GetAsync("/contacts/"+contact.Id);
+            var result = await _client.GetAsync("/contacts/1");
             result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task GetContactById_ForNonExistId_ReturnStatusNotFound()
+        {
+            var result = await _client.GetAsync($"/contacts/");
+            result.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
         }
 
         [Fact]
         public async Task DeleteContact_ForExistId_ReturnNonContent()
         {
-            var contact = new Contact()
-            {
-                Id = 2,
-            };
-            var result = await _client.DeleteAsync("/contacts"+contact.Id);
+            var result = await _client.DeleteAsync("/contacts/2");
             result.StatusCode.Should().Be(System.Net.HttpStatusCode.NoContent);
         }
 
@@ -93,7 +101,7 @@ namespace ContactsBook.IntegrationTests
 
             contact.Name = "Paweł";
             var httpContent = contact.ToJsonHttpContent();
-            var result = await _client.PutAsync("/contacts/2",httpContent);
+            var result = await _client.PutAsync("/contacts/2", httpContent);
             result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
         }
     }
